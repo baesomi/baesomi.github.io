996fa83d141c276c76266184f40f3a186960220f
BAESOMI <smbae0249@gmail.com>
2023-05-07 17:34:26 +0900

    about 페이지 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -43,14 +43,47 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2022.05 ~',
+        activity: 'NHN Dooray!',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: '',
+          demo: '',
         },
       },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2021.09 ~ 2022.04',
+        activity: 'NEXON',
+        links: {
+          post: '',
+          github: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2017.12 ~ 2021.09',
+        activity: '핸디소프트',
+        links: {
+          post: '',
+          github: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2014.03 ~ 2018.02',
+        activity: '가천대학교 소프트웨어설계경영',
+        links: {
+          post: '',
+          github: '',
+          demo: '',
+        },
+      }
     ],
 
     projects: [
@@ -72,15 +105,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: '',
+          demo: '',
         },
       },
     ],
